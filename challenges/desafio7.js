287acaa690b30751cb090936a2637200451c5647
Marco Viana <marcoviana100@gmail.com>
2023-03-15 19:09:16 -0300

    feat: Mostrar os produtos com vendidos diferentes de 50 e onde a tag não existe req.7

---
+++ challenges/desafio7.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+    { 
+        $and: [
+            {
+        vendidos: 
+        {
+            $ne: 50
+        }
+        },
+        {
+        tags: { $exists: false }
+        }
+        ]
+    }).projection({ _id: 0, nome: 1, vendidos: 1});
