fcaaf96da27e6191e9181ff8d19a74021c233d0b
Marco Viana <marcoviana100@gmail.com>
2023-03-19 23:43:37 -0300

    feat: add o campo vendasPorDia e incrementando como pedido no readMe req.22

---
+++ challenges/desafio22.js
@@ -0,0 +1,27 @@
+//1
+db.produtos.updateMany({}, {
+    $set: { 
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0]
+       }
+  });
+  
+  // 2
+  db.produtos.updateOne({
+      nome: 'Big Mac'
+  }, {
+      $inc: {
+          'vendasPorDia.3': 60
+      }
+  })
+  
+  // 3
+  db.produtos.updateMany({
+      tags: 'bovino'
+  }, {
+      $inc: {
+          'vendasPorDia.6': 120
+      }
+  })
+  
+  // Query 4
+  db.produtos.find().projection({ _id: false, nome: true, vendasPorDia: true });
