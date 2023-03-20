dd8d1059a678076d405c16811779785ee52af612
Marco Viana <marcoviana100@gmail.com>
2023-03-20 00:43:09 -0300

    feat: retornando produtos que possuem MC no nome req.27

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -2,17 +2,17 @@ db.produtos.updateMany({
     $and: [{
     valoresNutricionais: {
         $elemMatch: {
-            tipo: 'sódio',
-            percentual: { $gt: 20, $lt: 40 }
-        }
-    }
+            tipo: "sódio",
+            percentual: { $gt: 20, $lt: 40 },
+        },
     },
-    ]
+    },
+    ],
 
 }, {
    $push: {
-    tags: 'contém sódio'
-   }
+    tags: "contém sódio",
+   },
    });
 
 db.produtos.find().projection({ _id: false, nome: true, tags: true });
